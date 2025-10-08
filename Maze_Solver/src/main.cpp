ad6289590e7501b078d8f6883a84ac0b3730ad58
Kabilan K. <kalaichelvankabilan19@gmail.com>
2025-10-08 17:41:12 +0530

    Update main.cpp

--- Maze_Solver/src/main.cpp
+++ Maze_Solver/src/main.cpp
@@ -1,259 +1,222 @@
 #include <Arduino.h>
 
-// --- Ultrasonic sensor pins ---
-#define TRIG_PIN_FRONT 32
-#define ECHO_PIN_FRONT 33
-#define TRIG_PIN_LEFT 30
-#define ECHO_PIN_LEFT 31
-#define TRIG_PIN_RIGHT 36
-#define ECHO_PIN_RIGHT 37
+// --- Motor driver pins ---
+#define RPWM_L 6
+#define LPWM_L 7
+#define RPWM_R 10
+#define LPWM_R 9
+
+// --- Motor driver enable pins ---
+#define R_EN_L 22
+#define L_EN_L 23
+#define R_EN_R 24
+#define L_EN_R 25
 
 // --- Encoder pins ---
-#define ENCO_A_L 2
-#define ENCO_B_L 3
-#define ENCO_A_R 11
-#define ENCO_B_R 12
+#define ENCO_A_L 11
+#define ENCO_B_L 12
+#define ENCO_A_R 2
+#define ENCO_B_R 3
 
-// --- Motor driver pins ---
-const int RPWM_L = 5;
-const int LPWM_L = 6;
-const int RPWM_R = 7;
-const int LPWM_R = 8;
+// --- Ultrasonic sensor pins ---
+#define TRIG_FRONT 32
+#define ECHO_FRONT 33
+#define TRIG_LEFT 30
+#define ECHO_LEFT 31
+#define TRIG_RIGHT 36
+#define ECHO_RIGHT 37
 
-const int R_EN_L = 22;
-const int L_EN_L = 23;
-const int R_EN_R = 24;
-const int L_EN_R = 25;
+// --- Variables ---
+volatile long encoderCountLeft = 0;
+volatile long encoderCountRight = 0;
 
-// --- IR line sensor pins ---
-const int IR_PINS[8] = {40, 41, 42, 43, 44, 45, 46, 47};
-const int IR_ENABLE_PIN = 48;
+int baseSpeedL = 60;
+int baseSpeedR = 60;
 
-// --- Encoder Variables ---
-volatile long encoderCount_L = 0;
-volatile long encoderCount_R = 0;
-const int TICKS_PER_REV = 220;
+unsigned long lastTime = 0;
+unsigned long interval = 1000;
 
-// --- Ultrasonic Distances ---
-int distance_FRONT = 0;
-int distance_LEFT = 0;
-int distance_RIGHT = 0;
-
-// ------------------ Function Prototypes ------------------
-void moveForward(int SPEED);
-void turnLeft(int SPEED);
-void turnRight(int SPEED);
-void stopMotors();
-void readLineSensors(int sensors[]);
-void lineFollowContinuous();
-int getDistance(int trigPin, int echoPin);
-void encoderISR_L();
-void encoderISR_R();
-
-// ------------------ Encoder ISRs ------------------
-void encoderISR_L()
-{
-  encoderCount_L++;
+// --- Encoder ISRs ---
+void readEncoderLeft() {
+  int b = digitalRead(ENCO_B_L);
+  if (b == HIGH) encoderCountLeft++;
+  else encoderCountLeft--;
 }
 
-void encoderISR_R()
-{
-  encoderCount_R++;
+void readEncoderRight() {
+  int b = digitalRead(ENCO_B_R);
+  if (b == HIGH) encoderCountRight++;
+  else encoderCountRight--;
 }
 
-// ------------------ Motor Control ------------------
-void moveForward(int SPEED)
-{
-  analogWrite(RPWM_L, SPEED);
-  analogWrite(LPWM_L, 0);
-  analogWrite(RPWM_R, SPEED);
-  analogWrite(LPWM_R, 0);
-}
+// --- Motor Control ---
+void setMotorSpeeds(int leftSpeed, int rightSpeed) {
+  leftSpeed = constrain(leftSpeed, -255, 255);
+  rightSpeed = constrain(rightSpeed, -255, 255);
 
-void turnLeft(int SPEED)
-{
-  analogWrite(RPWM_L, 0);
-  analogWrite(LPWM_L, SPEED);
-  analogWrite(RPWM_R, SPEED);
-  analogWrite(LPWM_R, 0);
-}
+  // ✅ Adjusted for correct polarity (LEFT motor forward = RPWM_L)
+  if (leftSpeed >= 0) {
+    analogWrite(RPWM_L, leftSpeed);
+    analogWrite(LPWM_L, 0);
+  } else {
+    analogWrite(RPWM_L, 0);
+    analogWrite(LPWM_L, -leftSpeed);
+  }
 
-void turnRight(int SPEED)
-{
-  analogWrite(RPWM_L, SPEED);
-  analogWrite(LPWM_L, 0);
-  analogWrite(RPWM_R, 0);
-  analogWrite(LPWM_R, SPEED);
-}
-
-void stopMotors()
-{
-  analogWrite(RPWM_L, 0);
-  analogWrite(LPWM_L, 0);
-  analogWrite(RPWM_R, 0);
-  analogWrite(LPWM_R, 0);
-}
-
-// ------------------ Line Sensor Reading ------------------
-void readLineSensors(int sensors[])
-{
-  for (int i = 0; i < 8; i++)
-  {
-    sensors[i] = digitalRead(IR_PINS[i]);
+  // ✅ Adjusted for correct polarity (RIGHT motor forward = LPWM_R)
+  if (rightSpeed >= 0) {
+    analogWrite(LPWM_R, rightSpeed);
+    analogWrite(RPWM_R, 0);
+  } else {
+    analogWrite(LPWM_R, 0);
+    analogWrite(RPWM_R, -rightSpeed);
   }
 }
 
-// ------------------ Ultrasonic Distance ------------------
-int getDistance(int trigPin, int echoPin)
-{
+void stopMotors() {
+  analogWrite(RPWM_L, 0);
+  analogWrite(LPWM_L, 0);
+  analogWrite(RPWM_R, 0);
+  analogWrite(LPWM_R, 0);
+}
+
+// --- Ultrasonic Sensor Function ---
+long getDistance(int trigPin, int echoPin) {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-
-  long duration = pulseIn(echoPin, HIGH, 20000);
-  int distance = duration * 0.034 / 2;
-
+  long duration = pulseIn(echoPin, HIGH, 30000);
+  long distance = duration * 0.034 / 2;
+  if (distance == 0) distance = 300;
   return distance;
 }
 
-// ------------------ Line Following ------------------
-void lineFollowContinuous()
-{
-  int sensors[8];
-  static int lastError = 0;
-
-  readLineSensors(sensors);
-
-  int pos = 0, count = 0;
-  for (int i = 0; i < 8; i++)
-  {
-    if (sensors[i] == 0)
-    { // Line detected (black line)
-      pos += i * 100;
-      count++;
-    }
-  }
-
-  if (count > 0)
-  {
-    int avg = pos / count;
-    int error = avg - 350;
-
-    float Kp = 0.2, Kd = 1.0;
-    int derivative = error - lastError;
-    int correction = Kp * error + Kd * derivative;
-
-    int base = 150;
-    int leftPWM = base - correction;
-    int rightPWM = base + correction;
-
-    analogWrite(RPWM_L, constrain(leftPWM, 0, 255));
-    analogWrite(LPWM_L, 0);
-    analogWrite(RPWM_R, constrain(rightPWM, 0, 255));
-    analogWrite(LPWM_R, 0);
-
-    lastError = error;
-  }
-  else
-  {
-    stopMotors();
-  }
-
-  // Print IR sensor values for debugging
-  Serial.print("IR Sensors: ");
-  for (int i = 0; i < 8; i++)
-  {
-    Serial.print(sensors[i]);
-    Serial.print("\t");
-  }
-  Serial.println();
-}
-
-// ------------------ Setup ------------------
-void setup()
-{
+// --- Setup ---
+void setup() {
   Serial.begin(9600);
 
-  // Ultrasonic pins
-  pinMode(TRIG_PIN_FRONT, OUTPUT);
-  pinMode(ECHO_PIN_FRONT, INPUT);
-  pinMode(TRIG_PIN_LEFT, OUTPUT);
-  pinMode(ECHO_PIN_LEFT, INPUT);
-  pinMode(TRIG_PIN_RIGHT, OUTPUT);
-  pinMode(ECHO_PIN_RIGHT, INPUT);
-
-  // IR sensors
-  for (int i = 0; i < 8; i++)
-  {
-    pinMode(IR_PINS[i], INPUT);
-  }
-  pinMode(IR_ENABLE_PIN, OUTPUT);
-  digitalWrite(IR_ENABLE_PIN, HIGH);
-
-  // Motor driver
   pinMode(RPWM_L, OUTPUT);
   pinMode(LPWM_L, OUTPUT);
   pinMode(RPWM_R, OUTPUT);
   pinMode(LPWM_R, OUTPUT);
+
   pinMode(R_EN_L, OUTPUT);
   pinMode(L_EN_L, OUTPUT);
   pinMode(R_EN_R, OUTPUT);
   pinMode(L_EN_R, OUTPUT);
 
-  // Enable motor drivers
   digitalWrite(R_EN_L, HIGH);
   digitalWrite(L_EN_L, HIGH);
   digitalWrite(R_EN_R, HIGH);
   digitalWrite(L_EN_R, HIGH);
 
-  // Encoder interrupts
-  attachInterrupt(digitalPinToInterrupt(ENCO_A_L), encoderISR_L, RISING);
-  attachInterrupt(digitalPinToInterrupt(ENCO_A_R), encoderISR_R, RISING);
+  pinMode(ENCO_A_L, INPUT_PULLUP);
+  pinMode(ENCO_B_L, INPUT_PULLUP);
+  pinMode(ENCO_A_R, INPUT_PULLUP);
+  pinMode(ENCO_B_R, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(ENCO_A_L), readEncoderLeft, RISING);
+  attachInterrupt(digitalPinToInterrupt(ENCO_A_R), readEncoderRight, RISING);
+
+  pinMode(TRIG_FRONT, OUTPUT);
+  pinMode(ECHO_FRONT, INPUT);
+  pinMode(TRIG_LEFT, OUTPUT);
+  pinMode(ECHO_LEFT, INPUT);
+  pinMode(TRIG_RIGHT, OUTPUT);
+  pinMode(ECHO_RIGHT, INPUT);
+
+  Serial.println("Maze Robot Ready...");
+  lastTime = millis();
 }
 
-// ------------------ Main Loop ------------------
-void loop()
-{
-  // --- Ultrasonic Sensor Readings ---
-  distance_LEFT = getDistance(TRIG_PIN_LEFT, ECHO_PIN_LEFT);
-  delay(50);
-  distance_RIGHT = getDistance(TRIG_PIN_RIGHT, ECHO_PIN_RIGHT);
-  delay(50);
-  distance_FRONT = getDistance(TRIG_PIN_FRONT, ECHO_PIN_FRONT);
-  delay(50);
+// --- Movement helpers ---
+void moveForwardForTime(int speedL, int speedR, int durationMs) {
+  setMotorSpeeds(speedL, speedR);
+  delay(durationMs);
+  stopMotors();
+}
 
-  // --- Print Ultrasonic Readings ---
-  Serial.print("Front: ");
-  Serial.print(distance_FRONT);
-  Serial.print(" cm | Left: ");
-  Serial.print(distance_LEFT);
-  Serial.print(" cm | Right: ");
-  Serial.print(distance_RIGHT);
-  Serial.println(" cm");
+void turnRight90(int speed, int durationMs) {
+  analogWrite(LPWM_L, speed);
+  analogWrite(RPWM_L, 0);
+  analogWrite(LPWM_R, 0);
+  analogWrite(RPWM_R, speed);
+  delay(durationMs);
+  stopMotors();
+}
 
-  // --- Example Movement Logic ---
-  if (distance_FRONT < 10)
-  {
+void turnLeft90(int speed, int durationMs) {
+  analogWrite(LPWM_L, 0);
+  analogWrite(RPWM_L, speed);
+  analogWrite(LPWM_R, speed);
+  analogWrite(RPWM_R, 0);
+  delay(durationMs);
+  stopMotors();
+}
+
+// --- Main Loop ---
+void loop() {
+  long distFront = getDistance(TRIG_FRONT, ECHO_FRONT);
+  long distLeft = getDistance(TRIG_LEFT, ECHO_LEFT);
+  long distRight = getDistance(TRIG_RIGHT, ECHO_RIGHT);
+
+  Serial.print("F:");
+  Serial.print(distFront);
+  Serial.print(" L:");
+  Serial.print(distLeft);
+  Serial.print(" R:");
+  Serial.println(distRight);
+
+  // --- Case 1: Front and left blocked ---
+  if (distFront < 10 && distLeft < 10) {
+    Serial.println("Front & Left blocked -> move forward and turn right");
     stopMotors();
-    Serial.println("Obstacle Ahead!");
-  }
-  else if (distance_LEFT < 10)
-  {
-    turnRight(150);
-  }
-  else if (distance_RIGHT < 10)
-  {
-    turnLeft(150);
-  }
-  else
-  {
-    moveForward(150);
+    delay(200);
+    moveForwardForTime(40, 400, 400);
+    turnRight90(40, 600);
   }
 
-  // --- Uncomment to enable line following ---
-  // lineFollowContinuous();
+  // --- Case 2: Front only blocked ---
+  else if (distFront < 7) {
+    stopMotors();
+    delay(800);
+    if (distRight > 25) turnRight90(40, 600);
+    else if (distLeft > 25) turnLeft90(40, 600);
+    else turnRight90(40, 600); // dead end
+  }
 
-  delay(200);
+  // --- Case 3: Path correction ---
+  else if (distFront > 20){
+    long diff = distLeft - distRight;
+    int correction = diff * 1.5;
+    correction = constrain(correction, -30, 30);
+
+    int adjLeft = baseSpeedL - correction;
+    int adjRight = baseSpeedR + correction;
+
+    adjLeft = constrain(adjLeft, 0, 255);
+    adjRight = constrain(adjRight, 0, 255);
+
+    setMotorSpeeds(adjLeft, adjRight);
+  }
+
+  // --- Print encoder data every second ---
+  unsigned long currentTime = millis();
+  if (currentTime - lastTime >= interval) {
+    noInterrupts();
+    long leftCount = encoderCountLeft;
+    long rightCount = encoderCountRight;
+    encoderCountLeft = 0;
+    encoderCountRight = 0;
+    interrupts();
+
+    Serial.print("Encoders -> L:");
+    Serial.print(leftCount);
+    Serial.print("  R:");
+    Serial.println(rightCount);
+
+    lastTime = currentTime;
+  }
 }
